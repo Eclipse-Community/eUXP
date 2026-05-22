684ffd0eff0a9a25ccfa80b5cab6902a757e3d0c
the_r3dacted <k4sum1@eclipse.cx>
2026-05-21 20:10:57 -0700

    Revert "Issue #1830 - Remove --disable-xul config and conditionals."
    This reverts commit ddaee7c1be914cc28a28cbe27e5922215b3b2e6f.

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -901,7 +901,9 @@ protected:
   };
   PriorityParsingStatus ParsePriority();
 
+#ifdef MOZ_XUL
   bool ParseTreePseudoElement(nsAtomList **aPseudoElementArgs);
+#endif
 
   // Property specific parsing routines
   bool ParseImageLayers(const nsCSSPropertyID aTable[]);
@@ -6619,6 +6621,7 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
     }
   }
 
+#ifdef MOZ_XUL
   isTreePseudo = (pseudoElementType == CSSPseudoElementType::XULTree);
   // If a tree pseudo-element is using the function syntax, it will
   // get isTree set here and will pass the check below that only
@@ -6628,7 +6631,7 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
   // pseudo-elements are allowed to be either functions or not, as
   // desired.
   bool isTree = (eCSSToken_Function == mToken.mType) && isTreePseudo;
-
+#endif
   bool isPseudoElement = (pseudoElementType < CSSPseudoElementType::Count);
   // anonymous boxes are only allowed if they're the tree boxes or we have
   // enabled agent rules
@@ -6654,7 +6657,10 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
   // If it's a function token, it better be on our "ok" list, and if the name
   // is that of a function pseudo it better be a function token
   if ((eCSSToken_Function == mToken.mType) !=
-      (isTree ||
+      (
+#ifdef MOZ_XUL
+       isTree ||
+#endif
        nsCSSPseudoClasses::HasStringArg(pseudoClassType) ||
        nsCSSPseudoClasses::HasNthPairArg(pseudoClassType) ||
        nsCSSPseudoClasses::HasSelectorListArg(pseudoClassType)) &&
@@ -6818,8 +6824,11 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
     // various -moz-* pseudo-elements) must have |parsingPseudoElement|
     // set.
     if (!parsingPseudoElement &&
-        !nsCSSPseudoElements::IsCSS2PseudoElement(pseudo) &&
-        !isTreePseudo) {
+        !nsCSSPseudoElements::IsCSS2PseudoElement(pseudo)
+#ifdef MOZ_XUL
+        && !isTreePseudo
+#endif
+        ) {
       REPORT_UNEXPECTED_TOKEN(PEPseudoSelNewStyleOnly);
       UngetToken();
       return eSelectorParsingStatus_Error;
@@ -6830,6 +6839,7 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
       NS_ADDREF(*aPseudoElement = pseudo);
       *aPseudoElementType = pseudoElementType;
 
+#ifdef MOZ_XUL
       if (isTree) {
         // We have encountered a pseudoelement of the form
         // -moz-tree-xxxx(a,b,c).  We parse (a,b,c) and add each
@@ -6839,6 +6849,7 @@ CSSParserImpl::ParsePseudoSelector(int32_t&              aDataMask,
           return eSelectorParsingStatus_Error;
         }
       }
+#endif
 
       // Pseudo-elements can only be followed by user action pseudo-classes
       // or be the end of the selector.  So the next non-whitespace token must
@@ -7902,6 +7913,7 @@ CSSParserImpl::ParseRGBColor(ComponentType& aR,
   return false;
 }
 
+#ifdef MOZ_XUL
 bool
 CSSParserImpl::ParseTreePseudoElement(nsAtomList **aPseudoElementArgs)
 {
@@ -7927,6 +7939,7 @@ CSSParserImpl::ParseTreePseudoElement(nsAtomList **aPseudoElementArgs)
   fakeSelector.mClassList = nullptr;
   return true;
 }
+#endif
 
 nsCSSKeyword
 CSSParserImpl::LookupKeywordPrefixAware(nsAString& aKeywordStr,
