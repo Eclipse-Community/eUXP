b15e9969073c88947b92497c6285a34b98476831
the_r3dacted <k4sum1@eclipse.cx>
2026-05-21 18:12:33 -0700

    Revert "Issue #2342: Use [[nodiscard]] in /netwerk"
    This reverts commit d53992cc127fb85a63a23c01cd92347901bf2371.

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -3532,7 +3532,8 @@ ToIPCSegment(const nsStandardURL::URLSegment& aSegment)
     return ipc::StandardURLSegment(aSegment.mPos, aSegment.mLen);
 }
 
-[[nodiscard]] inline bool
+inline
+MOZ_MUST_USE bool
 FromIPCSegment(const nsACString& aSpec, const ipc::StandardURLSegment& aSegment, nsStandardURL::URLSegment& aTarget)
 {
     // This seems to be just an empty segment.
