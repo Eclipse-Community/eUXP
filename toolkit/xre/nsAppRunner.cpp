9659c95c33bdee5203e346b3507596ff5fa3859a
the_r3dacted <k4sum1@eclipse.cx>
2026-05-22 02:52:18 -0700

    Revert "Disallow E10S to run."
    This reverts commit 21f5ffe826a1c776d25354c01697ff1bff9b04e9.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4169,6 +4169,7 @@ MultiprocessBlockPolicy() {
 
   if (addonsCanDisable && disabledByAddons) {
     gMultiprocessBlockPolicy = kE10sDisabledForAddons;
+    return gMultiprocessBlockPolicy;
   }
 
 #if defined(XP_WIN)
@@ -4202,13 +4203,16 @@ MultiprocessBlockPolicy() {
 
   if (disabledForA11y) {
     gMultiprocessBlockPolicy = kE10sDisabledForAccessibility;
+    return gMultiprocessBlockPolicy;
   }
 #endif
-  
-  // We do not support E10S, block by policy.
-  gMultiprocessBlockPolicy = kE10sForceDisabled;
 
-  return gMultiprocessBlockPolicy;
+  /*
+   * None of the blocking policies matched, so e10s is allowed to run.
+   * Cache the information and return 0, indicating success.
+   */
+  gMultiprocessBlockPolicy = 0;
+  return 0;
 }
 
 bool
