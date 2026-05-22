002d19fbf9a834791fe6a0a85e3cc8f8f9a1b522
the_r3dacted <k4sum1@eclipse.cx>
2026-05-21 20:58:05 -0700

    Revert "Issue #3068 - Fix non-English MSVC text corruption."
    This reverts commit 993f6947d0859fb4b0aa93668be8ff8f47ed37cc.

--- config/expandlibs_exec.py
+++ config/expandlibs_exec.py
@@ -345,8 +345,8 @@ def main(args, proc_callback=None):
         (stdout, stderr) = proc.communicate()
         if proc.returncode and not options.verbose:
             print_command(sys.stderr, args)
-        sys.stderr.buffer.write(stdout)
-        sys.stderr.buffer.flush()
+        sys.stderr.write(stdout.decode(encoding='utf-8'))
+        sys.stderr.flush()
         if proc.returncode:
             return proc.returncode
         return 0
