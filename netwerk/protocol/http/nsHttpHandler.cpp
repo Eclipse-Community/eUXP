c762f43db3c06c6f3ec7c933eb72febced4b6450
the_r3dacted <k4sum1@eclipse.cx>
2026-05-23 00:44:10 -0700

    Revert "Issue #1741 - Follow-up: Remove superfluous prefobserver"
    This reverts commit 9fa7943937dfef58698f9955697509e6d649522f.

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -275,6 +275,7 @@ nsHttpHandler::Init()
     nsCOMPtr<nsIPrefBranch> prefBranch = do_GetService(NS_PREFSERVICE_CONTRACTID);
     if (prefBranch) {
         prefBranch->AddObserver(HTTP_PREF_PREFIX, this, true);
+        prefBranch->AddObserver(UA_PREF_PREFIX, this, true);
         prefBranch->AddObserver(GENERAL_UA_PREF_PREFIX, this, true);
         prefBranch->AddObserver(INTL_ACCEPT_LANGUAGES, this, true);
         prefBranch->AddObserver(BROWSER_PREF("disk_cache_ssl"), this, true);
